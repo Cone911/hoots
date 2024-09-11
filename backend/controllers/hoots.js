e17f8f59453ef12710ca980cd99742bc4275bec2
JP_Escobari <jpe911@gmail.com>
2024-09-11 13:25:19 -0400

    Index Functionality

--- backend/controllers/hoots.js
+++ backend/controllers/hoots.js
@@ -9,6 +9,18 @@ const router = express.Router();
 
 router.use(verifyToken);
 
+router.get('/', async (req, res) => {
+    try {
+      const hoots = await Hoot.find({})
+        .populate('author')
+        .sort({ createdAt: 'desc' });
+      res.status(200).json(hoots);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+});
+  
+
 router.post('/', async (req, res) => {
     try {
       req.body.author = req.user._id;
@@ -19,7 +31,8 @@ router.post('/', async (req, res) => {
       console.log(error);
       res.status(500).json(error);
     }
-  });
+});
+
   
 
 module.exports = router;
