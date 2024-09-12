7c80da6880ce59bc39011aed0ad5c2db2f2934b8
JP_Escobari <jpe911@gmail.com>
2024-09-12 10:43:15 -0400

    Render a List of Hoots

--- frontend/README.md
+++ frontend/README.md
@@ -8,3 +8,15 @@
 [ ] AAU, I should be able to add a comment to a specific hoot.
 [ ] As the author of a hoot, I should be able to update that hoot.
 [ ] As the author of a hoot, I should be able to delete that hoot.
+
+#Front-end
+## Below are the user stories we will implement within Hoot:
+
+[] As a guest, I should be able to create an account.
+[] As a guest with an account, I should be able to log in to my account.
+[] AAU, I should be able to create a hoot post.
+[] AAU, I should be able to see a list of all hoots on a ‘List’ page.
+[] AAU, clicking on a hoot in the ‘List’ page should navigate me to a ‘Details’ page where [] I can view information about a single hoot post along with its associated comments.
+[] AAU, I should be able to add a comment on a hoot ‘Details’ page.
+[] As the author of a hoot, I should see a link to ‘Edit’ a hoot on the ‘Details’ page. [] [] Clicking on the link should direct me to an ‘Edit’ page where I can modify the hoot. [] [] Upon submitting the update, I should be redirected back the the ‘Details’ page.
+[] As the author of a hoot, I should see a button to ‘Delete’ a hoot on the ‘Details’ page. [] Clicking on the button should delete the hoot, and redirect me back to the ‘List’ page.
--- frontend/src/pages/App/App.jsx
+++ frontend/src/pages/App/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react';
+import { useState, createContext, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { getUser } from '../../services/authService';
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar';
 import HomePage from '../HomePage/HomePage';
-import PostListPage from '../PostListPage/PostListPage';
+import HootListPage from '../HootListPage/HootListPage'
 import NewPostPage from '../NewPostPage/NewPostPage';
 import SignUpPage from '../SignUpPage/SignUpPage';
 import LogInPage from '../LogInPage/LogInPage';
+import * as hootService from '../../services/hootService';
 
 function App() {
   const [user, setUser] = useState(getUser());
+  const [hoots, setHoots] = useState([]);
+
+
+  useEffect(() => {
+    const fetchAllHoots = async () => {
+      const hootsData = await hootService.index();
+      setHoots(hootsData);
+    };
+    if (user) fetchAllHoots();
+  }, [user]);
 
   return (
     <main id="react-app">
@@ -19,8 +30,8 @@ function App() {
         {user ? (
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/posts" element={<PostListPage />} />
-            <Route path="/posts/new" element={<NewPostPage />} />
+            <Route path="/hoots" element={<HootListPage hoots={hoots}/>} />
+            <Route path="/hoots/new" element={<NewPostPage />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         ) : (
